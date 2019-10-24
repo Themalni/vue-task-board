e3cab56074dbe9a249a832504f6daa4b35a96205
Themalni <themalni@live.co.uk>
2019-10-24 16:31:41 +0200

    created card and column components

---
+++ src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card">
+    <div class="card-body">{{ description }}</div>
+    <div class="card-footer">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    description: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.card {
+  max-width: 100%;
+  height: 100px;
+  margin-bottom: 0.7em;
+  padding: 0.5em;
+  border-radius: 4px;
+  background-color: $white;
+  box-shadow: 0 1px 5px -3px $graphite;
+  font-size: 0.8em;
+}
+.card-footer {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist'
 
 Vue.use(Vuex)
 
+const vuexLocalStorage = new VuexPersist({ storage: localStorage })
+
 export default new Vuex.Store({
   state: {
-
+    tasks: []
+  },
+  getters: {
+    tasks: state => state.tasks
   },
   mutations: {
 
   },
   actions: {
 
-  }
+  },
+  plugins: [vuexLocalStorage.plugin]
 })
