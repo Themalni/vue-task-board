5573684e720ca29a6719b443a371e2b027c54332
Themalni <themalni@live.co.uk>
2019-10-28 16:08:24 +0100

    added closeDeleteModal function

--- src/components/task/ViewTaskModal.vue
+++ src/components/task/ViewTaskModal.vue
@@ -15,56 +15,56 @@
       </template>
     </Modal>
     <Modal title="Edytuj zadanie" :status="activeTask.status" v-else>
-    <template v-slot:body>
-      <div class="container pb-3">
-        <h4>Opis zadania:</h4>
-        <textarea
-          class="mt-2"
-          :class="[{ 'has-error': errors.first('description') }]"
-          name="description"
-          type="textarea"
-          label="Description"
-          v-model="activeTask.description"
-          v-validate="'required'"
-          data-vv-as="Opis zadania"
-          placeholder="Dodaj opis zadania..."
-          rows="6"
-          @focus="isEditing = true"
-        ></textarea>
-        <small
-          v-if="errors.first('description')"
-          class="text-xs error-text edit-error"
-        >{{ errors.first('description') }}</small>
-      </div>
-      <div class="container pb-3">
-        <h4 class="w-100">Rodzaj zadania:</h4>
-        <Select
-          :name="'taskTypesSelect'"
-          id="select"
-          label
-          v-model="activeTask.type"
-          rules="required"
-          friendly-name="Rodzaj zadania"
-          :options="taskTypes"
-          default-option="Wybierz rodzaj zadania"
-        ></Select>
-      </div>
-    </template>
-    <template v-slot:footer>
-      <Button class="button-left" color="button-danger" :disabled="!isEditing" @addAction="openDeleteTaskModal()">Usuń</Button>
-      <Button color="button-primary" :disabled="!isEditing" @addAction="setChanges(activeTask)">Zachowaj</Button>
-      <Button class="ml-2" color="button-default" @addAction="closeModal()">Anuluj</Button>
-    </template>
-  </Modal>
-  <Modal v-if="deleteModalIsOpen" title="Podtwierdzenie usuwania zadania" :small="true">
-    <template v-slot:body>
-      <h4>Czy napewno chcesz usunąć to zadanie?</h4>
-    </template>
-    <template v-slot:footer>
-      <Button color="button-danger" :disabled="!isEditing" @addAction="deleteSelectedTask(activeTask)">Usuń</Button>
-      <Button class="ml-2" color="button-default" @addAction="closeModal()">Anuluj</Button>
-    </template>
-  </Modal>
+      <template v-slot:body>
+        <div class="container pb-3">
+          <h4>Opis zadania:</h4>
+          <textarea
+            class="mt-2"
+            :class="[{ 'has-error': errors.first('description') }]"
+            name="description"
+            type="textarea"
+            label="Description"
+            v-model="activeTask.description"
+            v-validate="'required'"
+            data-vv-as="Opis zadania"
+            placeholder="Dodaj opis zadania..."
+            rows="6"
+            @focus="isEditing = true"
+          ></textarea>
+          <small
+            v-if="errors.first('description')"
+            class="text-xs error-text edit-error"
+          >{{ errors.first('description') }}</small>
+        </div>
+        <div class="container pb-3">
+          <h4 class="w-100">Rodzaj zadania:</h4>
+          <Select
+            :name="'taskTypesSelect'"
+            id="select"
+            label
+            v-model="activeTask.type"
+            rules="required"
+            friendly-name="Rodzaj zadania"
+            :options="taskTypes"
+            default-option="Wybierz rodzaj zadania"
+          ></Select>
+        </div>
+      </template>
+      <template v-slot:footer>
+        <Button class="button-left" color="button-danger" :disabled="!isEditing" @addAction="openDeleteTaskModal()">Usuń</Button>
+        <Button color="button-primary" :disabled="!isEditing" @addAction="setChanges(activeTask)">Zachowaj</Button>
+        <Button class="ml-2" color="button-default" @addAction="closeModal()">Anuluj</Button>
+      </template>
+    </Modal>
+    <Modal v-if="deleteModalIsOpen" title="Podtwierdzenie usuwania zadania" :small="true">
+      <template v-slot:body>
+        <h4>Czy napewno chcesz usunąć to zadanie?</h4>
+      </template>
+      <template v-slot:footer>
+        <Button color="button-danger" :disabled="!isEditing" @addAction="deleteSelectedTask(activeTask)">Usuń</Button>
+        <Button class="ml-2" color="button-default" @addAction="closeDeleteModal()">Anuluj</Button>
+      </template>
+    </Modal>
   </div>
 </template>
 
@@ -106,6 +106,9 @@ export default {
     ]),
     closeModal () {
       this.$store.dispatch('changeViewTaskModalState', false)
+    },
+    closeDeleteModal () {
+      this.deleteModalIsOpen = false
     },
     setChanges (taskToSave) {
       this.$store.dispatch('saveChanges', taskToSave)
